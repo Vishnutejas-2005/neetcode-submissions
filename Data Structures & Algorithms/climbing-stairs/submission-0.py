7d7b4c491fbe0ed06fdb6fc6dc18f7f36d039f5b
JS Vishnu Teja <144125542+Vishnutejas-2005@users.noreply.github.com>
2026-09-09 11:08:38 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        a = 1
+        b = 2
+
+        for i in range(3,n+1):
+            curr = a+b 
+            a = b
+            b = curr
+
+        return b
